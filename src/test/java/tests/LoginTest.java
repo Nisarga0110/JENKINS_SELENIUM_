764074b67fc35f995c297449e831d0aaaa44589b
NISARGA <nisarga7419@gmail.com>
2026-02-26 18:34:07 +0530

    new

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,24 +1,19 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import org.openqa.selenium.chrome.ChromeOptions;
+public class LoginTest   // ← make sure { is here
+{
 
-public class LoginTest {
-    public static void main(String[] args) throws InterruptedException {
-    	 ChromeOptions options = new ChromeOptions();
-    	 options.addArguments("--incognito");//to remove popup
-     
-         WebDriver driver = new ChromeDriver(options);
+    @Test
+    public void testLogin()
+    {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(1000);
-        driver.findElement(By.id("login-button")).click();
+        System.out.println("Test executed");
+        driver.quit();
     }
-}
+
+}  
